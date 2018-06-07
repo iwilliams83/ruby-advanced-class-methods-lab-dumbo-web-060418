dd06dfed7dbb9fe76f800f8f8ee9739a1ed3a658
iwilliams83 <indirajamurath@gmail.com>
2018-06-07 21:42:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,6 +42,14 @@ class Song
     end
   end
 
+  def self.new_from_filename(file_name)
+    file_name = file_name.split(' - ')
+    song = self.new
+    song.name = (file_name[1].split('.'))[0]
+    song.artist_name = file_name[0]
+    song
+  end
+
   def self.create_from_filename(file_name)
     file_name = file_name.split(' - ')
     song = self.new
@@ -54,6 +62,8 @@ class Song
   def self.alphabetical
     @@all.sort_by{|song| song.name}
   end
+
+
 
   def save
     self.class.all << self
